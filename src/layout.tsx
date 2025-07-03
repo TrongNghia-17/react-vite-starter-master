4d97ca92dd3fb60cabdc9c2794b1244cafe7fff0
NghiaHT <huynhtrongnghia15n@gmail.com>
2025-07-03 09:27:52 +0700

    video 18

--- src/layout.tsx
+++ src/layout.tsx
@@ -1,10 +1,12 @@
-
+import { Outlet } from "react-router-dom";
+import AppHeader from "./components/layout/app.header";
 
 function Layout() {
 
   return (
     <>
-      Hello NghiaHT
+      <AppHeader />
+      <Outlet />
     </>
   )
 }
--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,38 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import Layout from './layout'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Layout />,
+    children: [
+      {
+        path: "/book",
+        element: <div>book world!</div>,
+      },
+      {
+        path: "/about",
+        element: <div>about world!</div>,
+      },
+    ]
+  },
+  {
+    path: "/login",
+    element: <div>login world!</div>,
+  }, {
+    path: "/register",
+    element: <div>register world!</div>,
+  },
+]);
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <Layout />
-  </StrictMode>,
+    {/* <Layout /> */}
+    <RouterProvider router={router} />
+  </StrictMode>
 )
